9d8fe4f819070626f9e9fa6d5aec0cce6c7bc877
MichealOnyemelonu <monyemelonu@gmail.com>
2026-07-14 18:29:06 +0200

    updated the function feature in project.js to get all projects along with their organization names

--- src/models/project.js
+++ src/models/project.js
@@ -1,15 +1,21 @@
 import db from './db.js'
 
-const getAllProject = async() => {
+const getAllProjects = async() => {
     const query = `
         SELECT organization_id, title, description, location, project_date
-      FROM public.project;
+      FROM public.service_project;
     `;
 
     const result = await db.query(query);
 
+  
+
     return result.rows;
 }
 
-export {getAllProject}  
+export {getAllProjects}  
+
+
+
+
 
