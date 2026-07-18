19651bfd1212759edae26c3e67e98f265d49b132
MichealOnyemelonu <monyemelonu@gmail.com>
2026-07-18 19:53:12 +0200

    renamed project.js to projects.js

---
+++ src/models/projects.js
@@ -0,0 +1,21 @@
+import db from './db.js'
+
+const getAllProjects = async() => {
+    const query = `
+        SELECT organization_id, title, description, location, project_date
+      FROM public.service_project;
+    `;
+
+    const result = await db.query(query);
+
+  
+
+    return result.rows;
+}
+
+export {getAllProjects}  
+
+
+
+
+
