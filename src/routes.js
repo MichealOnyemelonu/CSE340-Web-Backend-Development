aa477e5b08478290ca7d0539de7889ce585ceb3f
Micheal Onye <monyemelonu@gmail.com>
2026-07-29 23:54:50 +0200

    1.Add an Edit Link to the Organization Details page 2.Add the route handler and controller for the Edit Organization form 3.Create the Edit Organization View 4.Add the route handler and controller to process the Edit Organization form submission 5.Create the updateOrganization model function 6.Add Validation

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import { showOrganizationsPage } from './controllers/organizations.js';
 import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
 import { showCategoriesPage, showCategoryDetailsPage  } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
-import { showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm, organizationValidation } from './controllers/organizations.js';
+import { showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm, organizationValidation, showEditOrganizationForm, processEditOrganizationForm } from './controllers/organizations.js';
 
 const router = express.Router();
 
@@ -30,5 +30,14 @@ router.get('/new-organization', showNewOrganizationForm);
 
 // Route to handle new organization form submission
 router.post('/new-organization', organizationValidation, processNewOrganizationForm);
+
+// Route to display the edit organization form
+router.get('/edit-organization/:id', showEditOrganizationForm);
+
+// Route to handle the edit organization form submission
+router.post('/edit-organization/:id', processEditOrganizationForm);
+
+// Route to handle the edit organization form submission
+router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm);
 
 export default router;
