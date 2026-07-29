9de7bd7f97ba4b4e8ac421effafc94d484384b03
Micheal Onye <monyemelonu@gmail.com>
2026-07-29 22:16:05 +0200

    1. included a placeholder logo 2.Create the view for the new organization form 3.Create the controller and route to render this view 4.Add a link to your new form 6.Create the model for adding a new organization 7.Configure Express to 8.Set up the controller and route to handle the form POSTHandle POST Requests

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import { showOrganizationsPage } from './controllers/organizations.js';
 import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
 import { showCategoriesPage, showCategoryDetailsPage  } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
-import { showOrganizationDetailsPage } from './controllers/organizations.js';
+import { showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm } from './controllers/organizations.js';
 
 const router = express.Router();
 
@@ -17,10 +17,18 @@ router.get('/project/:id', showProjectDetailsPage);
 router.get('/categories', showCategoriesPage);
 router.get('/category/:id', showCategoryDetailsPage);
 
+
+
 // Route for organization details page
 router.get('/organization/:id', showOrganizationDetailsPage);
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
+
+// Route for new organization page
+router.get('/new-organization', showNewOrganizationForm);
+
+// Route to handle new organization form submission
+router.post('/new-organization', processNewOrganizationForm);
 
 export default router;
